dc9d86d1e043f6f579b3347b687cb77872615408
Jay Barr <jayrbarr@gmail.com>
2020-05-16 15:21:05 -0400

    Adds middle section to proxy (Jasper's)

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const app = express();
 const port = 3001;
 
 const overviewService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com:3000";
+const gamesService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com";
 const reviewsService = "http://ec2-52-91-162-130.compute-1.amazonaws.com:3003/";
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/overview/*', createProxyMiddleware({ target: overviewService, changeOrigin: true }));
+app.use('/games/*'), createProxyMiddleware({ target: gamesService, changeOrigin: true }));
 app.use('/reviews/*', createProxyMiddleware({ target: reviewsService, changeOrigin: true }));
 
 app.listen(port, () => {
