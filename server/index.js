738de26d23621bb64b15961f3106dcdfa06aa337
Jay Barr <jayrbarr@gmail.com>
2020-05-16 15:42:36 -0400

    Trying to deal with the error on Jasper's bundle

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 3001;
 
-const overviewService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com:3000";
-const gamesService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com:80";
+const overviewService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com:3000/";
+const gamesService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com:80/";
 const reviewsService = "http://ec2-52-91-162-130.compute-1.amazonaws.com:3003/";
 
 app.use(express.static(path.join(__dirname, '../public')));
