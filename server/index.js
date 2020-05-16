349c38f03093a1a4d8adb94e320830b3bce28990
Jay Barr <jayrbarr@gmail.com>
2020-05-16 09:44:43 -0400

    Adds middleware and some routing

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = 3001;
 
+const overviewService = "http://ec2-3-22-119-240.us-east-2.compute.amazonaws.com:3000";
+const reviewsService = "http://ec2-52-91-162-130.compute-1.amazonaws.com:3003/";
+
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/overview/*', createProxyMiddleware({ target: overviewService, changeOrigin: true }));
+app.use('/reviews/*', createProxyMiddleware({ target: reviewsService, changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Proxy server running at port ${port}`);
 });
  
-
